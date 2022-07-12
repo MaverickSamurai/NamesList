966df49aa783eab937060315b00129d5c26d74c4
MaverickSamurai <mirsamirba@code.edu.az>
2022-07-13 00:23:30 +0400

    CheckName

---
+++ CheckName/CheckName/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CheckName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> namelist = new List<string>() { "Mirsamir", "Sadiq", "Cavid", "Orxan", "Ramil", "Esqin", "Cabbar", "Abbas", "Fuad"};
+
+            TeacherName(CheckName, namelist);
+            
+            
+
+            
+        }
+
+        public static bool CheckName(string name)
+        {
+            return name == "Cavid";
+        }
+
+
+        public static void TeacherName(Predicate<string> predicate, List<string> checkname)
+        {
+            
+            foreach (var item in checkname)
+            {
+                if (predicate(item))
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+            
+        }
+
+
+
+    }
+
+
+
+   
+}
